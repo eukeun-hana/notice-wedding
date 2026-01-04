b5fb0362ab0444fb7cd42bf424a106d1a6c8f029
eukeun-hana <connetflix@naver.com>
2026-01-04 22:17:20 +0900

    edit snap

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,16 +1,17 @@
 import coverImage from "./cover.png"
 //썸네일
 import t1 from "./thumb/image1.webp"
-import t2 from "./thumb/image2.webp"
+//import t2 from "./thumb/image2.webp"
+import t2 from "./thumb/blank.png"
 import t3 from "./thumb/image3.webp"
 import t4 from "./thumb/image4.webp"
 import t5 from "./thumb/image5.webp"
-import t6 from "./thumb/image6.webp"
-import t7 from "./thumb/image7.webp"
+import t6 from "./thumb/blank.png"
+import t7 from "./thumb/blank.png"
 import t8 from "./thumb/image8.webp"
 import t9 from "./thumb/image9.webp"
 import t10 from "./thumb/image10.webp"
-import t11 from "./thumb/image11.webp"
+import t11 from "./thumb/blank.png"
 import t12 from "./thumb/image12.webp"
 import t13 from "./thumb/image13.webp"
 import t14 from "./thumb/image14.webp"
@@ -23,16 +24,16 @@ import t20 from "./thumb/image20.webp"
 
 //확대용
 import f1 from "./image1.webp"
-import f2 from "./image2.webp"
+import f2 from "./blank.png"
 import f3 from "./image3.webp"
 import f4 from "./image4.webp"
 import f5 from "./image5.webp"
-import f6 from "./image6.webp"
-import f7 from "./image7.webp"
+import f6 from "./blank.png"
+import f7 from "./blank.png"
 import f8 from "./image8.webp"
 import f9 from "./image9.webp"
 import f10 from "./image10.webp"
-import f11 from "./image11.webp"
+import f11 from "./blank.png"
 import f12 from "./image12.webp"
 import f13 from "./image13.webp"
 import f14 from "./image14.webp"
@@ -45,16 +46,16 @@ import f20 from "./image20.webp"
 
 //확대용
 import image1 from "./image1.png" //보정 요청
-import image2 from "./image2.png" //보정 요청
+import image2 from "./blank.png" //보정 요청
 import image3 from "./image3.png" //보정 요청
 import image4 from "./image4.png" //보정 요청
 import image5 from "./image5.png" //보정 요청
-import image6 from "./image6.png"
-import image7 from "./image7.png" //보정 요청
+import image6 from "./blank.png"
+import image7 from "./blank.png" //보정 요청
 import image8 from "./image8.png"
 import image9 from "./image9.png" //보정 요청
 import image10 from "./image10.png" //보정 요청
-import image11 from "./image11.png"
+import image11 from "./blank.png"
 import image12 from "./image12.png" //보정 요청
 import image13 from "./image13.png" //회전
 import image14 from "./image14.png" //보정 요청
